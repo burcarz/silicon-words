a59e8ece6d5f8c1f647fab0bd602c9714d42092f
burcar <zacharyburcar@gmail.com>
2022-01-11 13:13:47 -0500

    attempting to fix deploy

--- config/connection.js
+++ config/connection.js
@@ -4,12 +4,11 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 // create connection to our database
-const sequelize = process.env.JAWSDB_URL
-    ? new Sequelize(process.env.JAWSDB_URL)
+const sequelize = process.env.POSTGRE_URL
+    ? new Sequelize(process.env.POSTGRE_URL)
     : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-    host: 'ec2-3-214-190-189.compute-1.amazonaws.com',
-    dialect: 'mysql',
-    port: process.env.PORT || 38887
+    dialect: 'postgres',
+    port: process.env.PORT
 });
 
 module.exports = sequelize;
