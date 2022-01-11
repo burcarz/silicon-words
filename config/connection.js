dff1928d50ca1ba5d7c5ceb876132a10f2682bda
burcar <zacharyburcar@gmail.com>
2022-01-11 12:59:20 -0500

    test

--- config/connection.js
+++ config/connection.js
@@ -7,9 +7,9 @@ require('dotenv').config();
 const sequelize = process.env.JAWSDB_URL
     ? new Sequelize(process.env.JAWSDB_URL)
     : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-    host: 'localhost',
+    // host: 'ec2-3-214-190-189.compute-1.amazonaws.com',
     dialect: 'mysql',
-    port: 3306
+    port: process.env.PORT || 38887
 });
 
 module.exports = sequelize;
